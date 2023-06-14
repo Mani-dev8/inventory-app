7e3a05c8bb6fb475fadb3e57d558492cbedc4541
manish <manishsewamahto090802@gmail.com>
2023-06-14 14:31:55 +0530

    Product added

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -19,3 +19,49 @@ exports.getProduct = async (req, res) => {
     }
 }
 
+
+exports.deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("🚀 ~ file: categoriesController.js:24 ~ exports.deleteProduct ~ id   ~~~  :", id)
+        const product = await Product.findById(id);
+        console.log("🚀 ~ file: categoriesController.js:27 ~ exports.deleteProduct ~ product   ~~~  :", product)
+
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        await product.deleteOne();
+        res.status(200).json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting product:', error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
+exports.updatedProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, image, description, category, weight, dimensions, quantity } = req.body;
+
+        const product = await Product.findById(id);
+
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        product.title = title || product.title;
+        product.image = image || product.image;
+        product.description = description || product.description;
+        product.category = category || product.category;
+        product.weight = weight || product.weight;
+        product.dimensions = dimensions || product.dimensions;
+        product.quantity = quantity || product.quantity;
+
+        await product.save();
+
+        res.status(200).json({ message: 'Product updated successfully', data: product });
+    } catch (error) {
+        console.error('Error updating product:', error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+}
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { newProduct, getProduct } = require('../controllers/productsController');
+const { newProduct, getProduct, deleteProduct, updatedProduct } = require('../controllers/productsController');
 
 const router = express.Router()
 
 router.post('/products/new', newProduct)
 router.get('/products/get',getProduct )
-
+router.delete('/products/:id', deleteProduct);
+router.put('/products/update/:id', updatedProduct);
 module.exports = router
